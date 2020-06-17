6ecb9c6e2884cbeed136a74f237d5318968ca29d
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-17 10:31:10 +0900

    デプロイ

---
+++ src/main/resources/db/migration/V1__create_products_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `suger` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
