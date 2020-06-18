0fbbcaef3c35c26fb8db2c95aefa23a595c667f8
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-18 13:53:01 +0900

    マイグレーション

---
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `is_active` tinyint DEFAULT NULL,
+  `login_id` varchar(255) DEFAULT NULL,
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
