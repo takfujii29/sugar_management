e096ab26d16e6b673fd4f9735d4ae1a385a56747
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-20 09:31:03 +0900

    リターン指定

--- src/main/java/jp/co/aivick/sugar/controller/HomeController.java
+++ src/main/java/jp/co/aivick/sugar/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
 	@RequestMapping
 	public String home(@AuthenticationPrincipal User user, Model model) {
 		model.addAttribute("name", user.getUsername());
-		return "/home";
+		return "/home.html";
 	}
 }
